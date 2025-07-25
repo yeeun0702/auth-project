01aae41ad65b23db2c723907fa3491f86c967562
yeeun0702 <kimyeeun0702@naver.com>
2025-07-25 23:06:24 +0900

    🎉 [init] #1 프로젝트 초기 설정 및 배포 환경 구성

--- src/main/java/com/example/authproject/controller/HealthCheckController.java
+++ src/main/java/com/example/authproject/controller/HealthCheckController.java
@@ -1,4 +1,14 @@
 package com.example.authproject.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthCheckController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
 }
+
