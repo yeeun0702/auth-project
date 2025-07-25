86b9f0c6c8c4e21acf6504d973d39a3fecf28e23
yeeun0702 <kimyeeun0702@naver.com>
2025-07-26 02:12:25 +0900

    ✨ [feat] #3 테스트 API 구현

--- src/main/java/com/example/authproject/controller/HealthCheckController.java
+++ src/main/java/com/example/authproject/controller/HealthCheckController.java
@@ -1,14 +1,37 @@
 package com.example.authproject.controller;
 
+import com.example.authproject.common.exception.base.CustomException;
+import com.example.authproject.common.exception.code.ErrorCode;
+import com.example.authproject.common.exception.code.SuccessCode;
+import com.example.authproject.common.response.ApiResponseDto;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HealthCheckController {
 
     @GetMapping("/ping")
-    public String ping() {
-        return "pong";
+    public ApiResponseDto<String> ping() {
+        return ApiResponseDto.success(SuccessCode.OK, "pong");
     }
+
+    // 성공 테스트
+    @GetMapping("/success")
+    public ApiResponseDto<String> success() {
+        return ApiResponseDto.success(SuccessCode.OK, null);
+    }
+
+    // 실패 테스트
+    @GetMapping("/error")
+    public void error() {
+        throw new CustomException(ErrorCode.TEST_ERROR);
+    }
+
 }
 
